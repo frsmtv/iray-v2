440d5981b96e751f48dfd08ef015db5e8f793ba0
frsmtv <fsimitchiev@gmail.com>
2016-12-20 22:32:51 +0100

    waiting for first feedback

---
+++ dist/js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+
+
+    // **** Navbar reduce on scroll ****
+    var scroll_start = 0;
+    var startchange = $('body');
+    var offset = startchange.offset();
+    $(document).scroll(function() {
+        scroll_start = $(this).scrollTop();
+        if(scroll_start > offset.top) {
+            $('.logoSquare').css({
+                width: '25px',
+                height: '25px',
+                'font-size': '0.6rem',
+                color: 'rgba(0, 0, 0, 0)'
+        });
+
+        } else {
+            $('.logoSquare').css({
+                width: '60px',
+                height: '60px',
+                'font-size': '0.8rem',
+                color: '#fff'
+            });
+        }
+    });
+
+
+    // **** SCROLL TO TOP ****
+    $('#topBtn').click(function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+    });
+
+    // **** MEDIA SECTIONS REVEAL ****
+    $('#audioLink').click(function() {
+        $('#videos').hide();
+        $('#music').fadeIn();
+        $('html,body').animate({ scrollTop: 9999 }, 'slow');
+    });
+
+    $('#videoLink').click(function() {
+        $('#music').hide();
+        $('#videos').fadeIn();
+        $('html,body').animate({ scrollTop: 9999 }, 'slow');
+    });
+
+
+
+
+});
