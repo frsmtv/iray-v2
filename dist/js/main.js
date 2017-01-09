2ece92b9786fffc9045c2d4c020969c5c6697947
frsmtv <fsimitchiev@gmail.com>
2017-01-09 14:13:34 +0100

    ok

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
 
 
+    // **** INTRO ANIMATIONS ****
+    $('#intro').click(function() {
+       $('#introUp').addClass('animated fadeOutUp');
+       $('#introh1').addClass('animated fadeOutUp');
+       $('#introDown').addClass('animated fadeOutDown');
+       $('#introh2').addClass('animated fadeOutDown');
+    });
+
+
     // **** Navbar reduce on scroll ****
     var scroll_start = 0;
     var startchange = $('body');
@@ -12,23 +21,19 @@ $(document).ready(function(){
                 width: '25px',
                 height: '25px',
                 'font-size': '0.6rem',
-                color: 'rgba(0, 0, 0, 0)'
-            });
-            // home square color switch
-            $('#logoSquareBlack').attr('src', 'img/iray-logo-black.png')
+                // color: 'rgba(0, 0, 0, 0)'
+        });
+            // // home square color switch
+            // $('#logoSquareBlack').attr('src', 'dist/img/iray-logo-black.png')
 
         } else {
             $('.logoSquare').css({
                 width: '60px',
                 height: '60px',
                 'font-size': '0.8rem',
-                color: '#fff'
             });
-            $('#logoSquareBlack').attr('src', 'img/iray-logo-white.png')
         }
     });
-
-
 
 
     // **** SCROLL TO TOP ****
@@ -40,13 +45,11 @@ $(document).ready(function(){
     $('#audioLink').click(function() {
         $('#videos').hide();
         $('#music').fadeIn();
-        $('html,body').animate({ scrollTop: 9999 }, 'slow');
     });
 
     $('#videoLink').click(function() {
         $('#music').hide();
         $('#videos').fadeIn();
-        $('html,body').animate({ scrollTop: 9999 }, 'slow');
     });
 
 
